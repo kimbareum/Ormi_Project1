7abc3d6819a0cfbaed8ef86ed50beb2ae1b06ed6
kimbareum <right.kim93@gmail.com>
2023-06-01 09:07:02 +0900

    다크모드 JS코드 불필요한부분 제거

--- esset/JS/utils.js
+++ esset/JS/utils.js
@@ -11,20 +11,17 @@ const url = `https://estsoft-openai-api.jejucodingcamp.workers.dev/`;
 // Darkmode 확인
 //////////////////////////////////////////
 
-const isDarkMode = () =>
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches;
-
 const runColorMode = (fn) => {
     if (!window.matchMedia) {
         return;
     }
-
     const query = window.matchMedia("(prefers-color-scheme: dark)");
 
     fn(query.matches);
 
-    query.addEventListener("change", (event) => fn(event.matches));
+    query.addEventListener("change", (event) => {
+        fn(event.matches);
+    });
 };
 
 runColorMode((isDarkMode) => {
